6f6888da57619a592612c27b0e768f56ac474a17
reginesgit <regine.strey@gmail.com>
2021-05-14 21:51:53 -0400

    Re-submitting after debug!

--- logic.js
+++ logic.js
@@ -1,11 +1,10 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-04-27"+ "&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
-// "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
+  console.log(data)
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
@@ -19,10 +18,47 @@ function createFeatures(earthquakeData) {
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
+  function setColor(depth){
+    if(depth > 80)
+    return "blue"
+    else if(depth > 70)
+    return "red"
+    else if(depth > 60)
+    return "orange"
+    else if(depth > 50)
+    return "yellow"
+    else if(depth > 40)
+    return "green"
+    else if(depth > 30)
+    return "purple"
+    else if(depth > 20)
+    return "pink"
+    else if(depth > 10)
+    return "lightblue"
+    else if(depth > 0)
+    return "aquamarine"
+    else
+    return "white"
+  }
+
+
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature,latlng){
+      console.log(feature)
+      return L.circleMarker(latlng)
+    },
+    style: function(feature){
+      return {
+        radius: feature.properties.mag*6,
+        color: "white",
+        fillOpacity: 1,
+        weight: 3,
+        fillColor: setColor(feature.geometry.coordinates[2])
+      }
+    }
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -60,7 +96,7 @@ function createMap(earthquakes) {
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("mapid", {
+  var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
@@ -74,4 +110,25 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+
+      var div = L.DomUtil.create('div', 'info legend'),
+          grades = [0, 10, 20, 30, 40, 50, 60, 70, 80],
+          colors = ["blue", "red", "orange", "yellow", "green", "purple","pink", "lightblue", "aquamarine"].reverse()
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + colors[i] + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+
+      return div;
+  };
+
+legend.addTo(myMap);
+
 }
--- static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoicmVnaW5lLXN0cmV5IiwiYSI6ImNrbnA2Y3ExMjFkNjAybnA4eGhkeXY4cDAifQ.hmhf9iig38QXcnzXuO67aA";
